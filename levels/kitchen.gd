036fa0baec3c2305be6f414ad0be75db89588547
Bob B <bobblinde@gmail.com>
2024-04-03 14:53:11 -0600

    adding actual scenes

--- levels/kitchen.gd
+++ levels/kitchen.gd
@@ -11,20 +11,21 @@ func _process(delta):
 	pass
 
 
-func _on_area_2d_mouse_entered():
-	$Door/DoorSprite.scale = Vector2(1.2,1.2)
-
 
 
 func _on_door_mouse_exited():
 	$Door/DoorSprite.scale = Vector2(1,1)
-	
 
+func _on_door_mouse_entered():
+	$Door/DoorSprite.scale = Vector2(1.2,1.2)
 
-func _on_audio_stream_player_2d_finished():
-	$AudioStreamPlayer2D.play()
+func _on_clock_mouse_exited():
+	$Clock/ClockSprite.scale = Vector2(1,1)
+
+func _on_clock_mouse_entered():
+	$Clock/ClockSprite.scale = Vector2(1.2,1.2)
 
 
 func _on_door_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
-		get_tree().change_scene_to_file("res://levels/kitchen.tscn")
+		get_tree().change_scene_to_file("res://levels/Bedroom.tscn")
